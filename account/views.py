181d6c2df496976543a3c7d3bf5b2037fac70403
Samuele <sam13centax@gmail.com>
2024-06-20 13:02:10 +0200

    aggiunte prime cartelle e file

--- account/views.py
+++ account/views.py
@@ -1,3 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None: # verifico che l'utente si sia autenticato con successo
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Autenticazione avvenuta con successo')
+                else:
+                    return HttpResponse('Errore nell\'autenticazione')
+            else:
+                return HttpResponse('Login non valido')
+    else:
+        form=LoginForm()
+    return render(request, 'account/login.html', {'form': form})
--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^account/', include('account.urls')),
 ]
